1426c5bc881325969d2b9809d974cf02ace8a053
Ramtin <kh.ramtin@gmail.com>
2017-05-08 20:19:14 +0430

    react-native-navigation Setup

--- android/app/src/main/java/com/simpletodo/MainActivity.java
+++ android/app/src/main/java/com/simpletodo/MainActivity.java
@@ -1,15 +1,15 @@
 package com.simpletodo;
 
-import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.NavigationApplication;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends NavigationApplication {
 
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
-    @Override
-    protected String getMainComponentName() {
-        return "SimpleTodo";
-    }
+    // @Override
+    // protected String getMainComponentName() {
+    //     return "SimpleTodo";
+    // }
 }
--- android/app/src/main/java/com/simpletodo/MainApplication.java
+++ android/app/src/main/java/com/simpletodo/MainApplication.java
@@ -2,7 +2,8 @@ package com.simpletodo;
 
 import android.app.Application;
 
-import com.facebook.react.ReactApplication;
+import com.reactnativenavigation.NavigationApplication;
+// import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -13,28 +14,49 @@ import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
 
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-      );
-    }
-  };
+	@Override
+	public boolean isDebug() {
+			// Make sure you are using BuildConfig from your own application
+			return BuildConfig.DEBUG;
+	}
 
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
+	@Override
+	public List<ReactPackage> createAdditionalReactPackages() {
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
+	// Add additional packages you require here
+	return Arrays.<ReactPackage>asList(
+			// eg. new VectorIconsPackage()
+	);
+
+	// No need to add RnnPackage and MainReactPackage
+	// Simply return null if you do not have additional packages:
+	// return null;
+	}
 }
+//
+//   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+//     @Override
+//     public boolean getUseDeveloperSupport() {
+//       return BuildConfig.DEBUG;
+//     }
+//
+//     @Override
+//     protected List<ReactPackage> getPackages() {
+//       return Arrays.<ReactPackage>asList(
+//           new MainReactPackage()
+//       );
+//     }
+//   };
+//
+//   @Override
+//   public ReactNativeHost getReactNativeHost() {
+//     return mReactNativeHost;
+//   }
+//
+//   @Override
+//   public void onCreate() {
+//     super.onCreate();
+//     SoLoader.init(this, /* native exopackage */ false);
+//   }
+// }

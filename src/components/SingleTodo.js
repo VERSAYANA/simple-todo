c4b1d6e1083e7ad16c576a5ef3f66d6d872b1dc5
Ramtin <kh.ramtin@gmail.com>
2017-07-23 22:25:39 +0430

    Animation

--- src/components/SingleTodo.js
+++ src/components/SingleTodo.js
@@ -1,111 +1,119 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import {
-  View,
-  Text,
-  TouchableNativeFeedback,
-  TextInput,
-  TouchableWithoutFeedback
-} from "react-native";
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+	NativeModules,
+	LayoutAnimation,
+	View,
+	Text,
+	TouchableNativeFeedback,
+	TextInput,
+	TouchableWithoutFeedback
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import Buttons from "./Buttons";
-import style from "./style/SingleTodo";
+import Buttons from './Buttons';
+import style from './style/SingleTodo';
+
+const { UIManager } = NativeModules;
+
+UIManager.setLayoutAnimationEnabledExperimental &&
+	UIManager.setLayoutAnimationEnabledExperimental(true);
 
 export default class SingleTodo extends React.Component {
-  render() {
-    const {
-      todo,
-      complete,
-      deleteTodo,
-      editMode,
-      textMode,
-      textTodo,
-      viewAdditionalNote,
-      dateTodo,
-      showListSelector,
-      toggleFocus
-    } = this.props;
+	render() {
+		const {
+			todo,
+			complete,
+			deleteTodo,
+			editMode,
+			textMode,
+			textTodo,
+			viewAdditionalNote,
+			dateTodo,
+			showListSelector,
+			toggleFocus
+		} = this.props;
 
-    let checkBox = null;
-    if (!todo.completed) {
-      checkBox = (
-        <Icon
-          name="checkbox-blank-outline"
-          size={22}
-          color="rgba(255, 255, 255, 0.80)"
-        />
-      );
-    } else {
-      checkBox = (
-        <Icon
-          name="checkbox-marked"
-          size={22}
-          color="rgba(255, 255, 255, 0.80"
-        />
-      );
-    }
+		let checkBox = null;
+		if (!todo.completed) {
+			checkBox = (
+				<Icon
+					name="checkbox-blank-outline"
+					size={22}
+					color="rgba(255, 255, 255, 0.80)"
+				/>
+			);
+		} else {
+			checkBox = (
+				<Icon
+					name="checkbox-marked"
+					size={22}
+					color="rgba(255, 255, 255, 0.80"
+				/>
+			);
+		}
 
-    let todoTitle = null;
-    if (!todo.editText) {
-      if (!todo.completed) {
-        todoTitle = (
-          <Text style={style.text}>
-            {todo.text}
-          </Text>
-        );
-      } else {
-        todoTitle = (
-          <Text style={style.completedText}>
-            {todo.text}
-          </Text>
-        );
-      }
-    } else {
-      todoTitle = (
-        <TextInput
-          style={style.textInput}
-          defaultValue={todo.text}
-          autoFocus={true}
-          onBlur={() => {
-            textMode(todo.id);
-            editMode(todo.id);
-          }}
-          returnKeyType={"done"}
-          onSubmitEditing={v => {
-            textTodo(v.nativeEvent.text, todo.id);
-            textMode(todo.id);
-            editMode(todo.id);
-          }}
-        />
-      );
-    }
+		let todoTitle = null;
+		if (!todo.editText) {
+			if (!todo.completed) {
+				todoTitle = (
+					<Text style={style.text}>
+						{todo.text}
+					</Text>
+				);
+			} else {
+				todoTitle = (
+					<Text style={style.completedText}>
+						{todo.text}
+					</Text>
+				);
+			}
+		} else {
+			todoTitle = (
+				<TextInput
+					style={style.textInput}
+					defaultValue={todo.text}
+					autoFocus={true}
+					onBlur={() => {
+						textMode(todo.id);
+						editMode(todo.id);
+					}}
+					returnKeyType={'done'}
+					onSubmitEditing={v => {
+						textTodo(v.nativeEvent.text, todo.id);
+						textMode(todo.id);
+						editMode(todo.id);
+					}}
+				/>
+			);
+		}
 
-    return (
-      <TouchableNativeFeedback
-        onPress={() => {
-          editMode(todo.id);
-        }}
-      >
-        <View style={style.todoWrapper}>
-          <View style={style.todoRow}>
-            <View style={style.left}>
-              <TouchableWithoutFeedback
-                onPress={() => {
-                  complete(todo.id);
-                }}
-              >
-                <View style={style.checkBox}>
-                  {checkBox}
-                </View>
-              </TouchableWithoutFeedback>
+		return (
+			<TouchableNativeFeedback
+				onPress={() => {
+					LayoutAnimation.easeInEaseOut();
+					editMode(todo.id);
+				}}
+			>
+				<View style={style.todoWrapper}>
+					<View style={style.todoRow}>
+						<View style={style.left}>
+							<TouchableWithoutFeedback
+								onPress={() => {
+									complete(todo.id);
+								}}
+							>
+								<View style={style.checkBox}>
+									{checkBox}
+								</View>
+							</TouchableWithoutFeedback>
 
-              <View style={style.textContainer}>
-                {todoTitle}
-              </View>
-            </View>
+							<View style={style.textContainer}>
+								{todoTitle}
+							</View>
+						</View>
 
-            {/* <View style={style.iconsContainer}> */}
-            {/* {todo.note
+						{/* <View style={style.iconsContainer}> */}
+						{/* {todo.note
                 ? <Icon
 										style={style.icon}
                     name="note-outline"
@@ -113,7 +121,7 @@ export default class SingleTodo extends React.Component {
                     color="rgba(0, 0, 0, 0.54)"
                   />
                 : null} */}
-            {/* {showDateIcon && todo.date
+						{/* {showDateIcon && todo.date
                 ? <Icon
 									style={style.icon}
                     name="calendar-blank"
@@ -121,39 +129,39 @@ export default class SingleTodo extends React.Component {
                     color="rgba(0, 0, 0, 0.54)"
                   />
                 : null} */}
-            <TouchableNativeFeedback onPress={() => toggleFocus(todo.id)}>
-              <View style={style.iconStar}>
-                {!todo.focus
-                  ? <Icon
-                      name="star-outline"
-                      size={22}
-                      color="rgba(255, 255, 255, 0.80)"
-                    />
-                  : <Icon
-                      name="star"
-                      size={22}
-                      color="rgba(255, 255, 255, 0.80)"
-                    />}
-              </View>
-            </TouchableNativeFeedback>
+						<TouchableNativeFeedback onPress={() => toggleFocus(todo.id)}>
+							<View style={style.iconStar}>
+								{!todo.focus
+									? <Icon
+											name="star-outline"
+											size={22}
+											color="rgba(255, 255, 255, 0.80)"
+										/>
+									: <Icon
+											name="star"
+											size={22}
+											color="rgba(255, 255, 255, 0.80)"
+										/>}
+							</View>
+						</TouchableNativeFeedback>
 
-            {/* </View> */}
-          </View>
-          {todo.editMode
-            ? <Buttons
-                deleteTodo={deleteTodo}
-                textMode={textMode}
-                id={todo.id}
-                note={todo.note}
-                date={todo.date}
-                list={todo.list}
-                viewAdditionalNote={viewAdditionalNote}
-                dateTodo={dateTodo}
-                showListSelector={showListSelector}
-              />
-            : null}
-        </View>
-      </TouchableNativeFeedback>
-    );
-  }
+						{/* </View> */}
+					</View>
+					{todo.editMode
+						? <Buttons
+								deleteTodo={deleteTodo}
+								textMode={textMode}
+								id={todo.id}
+								note={todo.note}
+								date={todo.date}
+								list={todo.list}
+								viewAdditionalNote={viewAdditionalNote}
+								dateTodo={dateTodo}
+								showListSelector={showListSelector}
+							/>
+						: null}
+				</View>
+			</TouchableNativeFeedback>
+		);
+	}
 }
--- src/screens/Week/Week.js
+++ src/screens/Week/Week.js
@@ -1,201 +1,213 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import {
-  ViewPagerAndroid,
-  View,
-  Text,
-  TouchableNativeFeedback,
-  ScrollView,
-  TextInput
-} from "react-native";
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+  NativeModules,
+  LayoutAnimation,
+	ViewPagerAndroid,
+	View,
+	Text,
+	TouchableNativeFeedback,
+	ScrollView,
+	TextInput
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import SingleTodo from "../../components/SingleTodo";
-import NewTodoInput from "../../components/NewTodoInput";
-import style from "./style/Week";
+import SingleTodo from '../../components/SingleTodo';
+import NewTodoInput from '../../components/NewTodoInput';
+import style from './style/Week';
+
+const { UIManager } = NativeModules;
+
+UIManager.setLayoutAnimationEnabledExperimental &&
+	UIManager.setLayoutAnimationEnabledExperimental(true);
 
 export default class Week extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    const todayIndex = new Date(props.today).getDay();
-    let array = [];
-    for (i = 0; i <= 6; i++) {
-      if (i === todayIndex) {
-        array.push(true);
-      } else array.push(false);
-    }
+		const todayIndex = new Date(props.today).getDay();
+		let array = [];
+		for (i = 0; i <= 6; i++) {
+			if (i === todayIndex) {
+				array.push(true);
+			} else array.push(false);
+		}
 
-    this.state = { show: array };
+		this.state = { show: array };
 
-    this._viewAdditionalNote = this._viewAdditionalNote.bind(this);
-    this._showListSelector = this._showListSelector.bind(this);
-  }
+		this._viewAdditionalNote = this._viewAdditionalNote.bind(this);
+		this._showListSelector = this._showListSelector.bind(this);
+	}
 
-  componentWillMount() {
-    const { today, dateToday } = this.props;
-    const dateNow = new Date();
+	componentWillMount() {
+		const { today, dateToday } = this.props;
+		const dateNow = new Date();
 
-    if (!today || today !== dateNow.toDateString()) {
-      dateToday(dateNow.toDateString());
-    }
-  }
+		if (!today || today !== dateNow.toDateString()) {
+			dateToday(dateNow.toDateString());
+		}
+	}
 
-  _viewAdditionalNote(id, note) {
-    this.props.navigator.showModal({
-      screen: "simpletodo.AdditionalNote",
-      title: "Note",
-      passProps: {
-        id,
-        note
-      },
-      navigatorStyle: {
-        statusBarColor: "#347f78",
-        navBarBackgroundColor: "#4d9891",
-        navBarTextColor: "white",
-        navBarButtonColor: "white",
-        screenBackgroundColor: "#FAFAFA"
-      },
-      navigatorButtons: {
-        rightButtons: [
-          {
-            id: "done-note",
-            icon: require("../../icons/check-74-white.png"),
-            buttonColor: "white"
-          }
-        ]
-      }
-    });
-  }
+	_viewAdditionalNote(id, note) {
+		this.props.navigator.showModal({
+			screen: 'simpletodo.AdditionalNote',
+			title: 'Note',
+			passProps: {
+				id,
+				note
+			},
+			navigatorStyle: {
+				statusBarColor: '#347f78',
+				navBarBackgroundColor: '#4d9891',
+				navBarTextColor: 'white',
+				navBarButtonColor: 'white',
+				screenBackgroundColor: '#FAFAFA'
+			},
+			navigatorButtons: {
+				rightButtons: [
+					{
+						id: 'done-note',
+						icon: require('../../icons/check-74-white.png'),
+						buttonColor: 'white'
+					}
+				]
+			}
+		});
+	}
 
-  _showListSelector(list, id) {
-    this.props.navigator.showLightBox({
-      screen: "simpletodo.ListSelector",
-      passProps: {
-        list,
-        id
-      },
-      style: {
-        backgroundBlur: "dark"
-      }
-    });
-  }
+	_showListSelector(list, id) {
+		this.props.navigator.showLightBox({
+			screen: 'simpletodo.ListSelector',
+			passProps: {
+				list,
+				id
+			},
+			style: {
+				backgroundBlur: 'dark'
+			}
+		});
+	}
 
-  _dayTitle(index, todayIndex) {
-    if (index === todayIndex) {
-      return "Today";
-    }
-    switch (index) {
-      case 0:
-        return "Sunday";
+	_dayTitle(index, todayIndex) {
+		if (index === todayIndex) {
+			return 'Today';
+		}
+		switch (index) {
+			case 0:
+				return 'Sunday';
 
-      case 1:
-        return "Monday";
-      case 2:
-        return "Tuesday";
-      case 3:
-        return "Wednesday";
-      case 4:
-        return "Thursday";
-      case 5:
-        return "Friday";
-      case 6:
-        return "Saturday";
-    }
-  }
+			case 1:
+				return 'Monday';
+			case 2:
+				return 'Tuesday';
+			case 3:
+				return 'Wednesday';
+			case 4:
+				return 'Thursday';
+			case 5:
+				return 'Friday';
+			case 6:
+				return 'Saturday';
+		}
+	}
 
-  _dayDate(index, todayIndex, today) {
-    const date = new Date(today);
-    date.setDate(date.getDate() - (todayIndex - index));
-    return date.toDateString();
-  }
+	_dayDate(index, todayIndex, today) {
+		const date = new Date(today);
+		date.setDate(date.getDate() - (todayIndex - index));
+		return date.toDateString();
+	}
 
-  _showDay(index) {
-    const array = this.state.show;
-    array[index] = !array[index];
-    this.setState({ show: array });
-  }
+	_showDay(index) {
+		const array = this.state.show;
+		array[index] = !array[index];
+		this.setState({ show: array });
+	}
 
-  render() {
-    const {
-      today,
-      week,
-      addTodo,
-      list,
-      id,
-      complete,
-      deleteTodo,
-      editMode,
-      textMode,
-      textTodo,
-      dateTodo,
-      toggleFocus
-    } = this.props;
-    const todayIndex = new Date(today).getDay();
+	render() {
+		const {
+			today,
+			week,
+			addTodo,
+			list,
+			id,
+			complete,
+			deleteTodo,
+			editMode,
+			textMode,
+			textTodo,
+			dateTodo,
+			toggleFocus
+		} = this.props;
+		const todayIndex = new Date(today).getDay();
 
-    return (
-      <ScrollView>
-        {week.map((dayTodos, index) =>
-          <View
-            key={index}
-            style={index !== 6 ? style.dayContainer : style.lastContainer}
-          >
-            <TouchableNativeFeedback onPress={() => this._showDay(index)}>
-              <View style={style.subheaderContainer}>
-                <Text
-                  style={
-                    index !== todayIndex
-                      ? style.subheader
-                      : style.todaySubheader
-                  }
-                >
-                  {this._dayTitle(index, todayIndex)}
-                </Text>
-                {!this.state.show[index]
-                  ? <Icon
-                      name={"chevron-down"}
-                      size={18}
-                      color={"rgba(255, 255, 255, 0.65)"}
-                    />
-                  : <Icon
-                      name={"chevron-up"}
-                      size={18}
-                      color={"rgba(255, 255, 255, 0.65)"}
-                    />}
-              </View>
-            </TouchableNativeFeedback>
+		return (
+			<ScrollView>
+				{week.map((dayTodos, index) =>
+					<View
+						key={index}
+						style={index !== 6 ? style.dayContainer : style.lastContainer}
+					>
+						<TouchableNativeFeedback
+							onPress={() => {
+								LayoutAnimation.easeInEaseOut();
+								this._showDay(index);
+							}}
+						>
+							<View style={style.subheaderContainer}>
+								<Text
+									style={
+										index !== todayIndex
+											? style.subheader
+											: style.todaySubheader
+									}
+								>
+									{this._dayTitle(index, todayIndex)}
+								</Text>
+								{!this.state.show[index]
+									? <Icon
+											name={'chevron-down'}
+											size={18}
+											color={'rgba(255, 255, 255, 0.65)'}
+										/>
+									: <Icon
+											name={'chevron-up'}
+											size={18}
+											color={'rgba(255, 255, 255, 0.65)'}
+										/>}
+							</View>
+						</TouchableNativeFeedback>
 
-            {this.state.show[index]
-              ? <View>
-                  <NewTodoInput
-                    addTodo={addTodo}
-                    list={list}
-                    date={this._dayDate(index, todayIndex, today)}
-                    focus={false}
-                    id={id}
-                  />
+						{this.state.show[index]
+							? <View>
+									<NewTodoInput
+										addTodo={addTodo}
+										list={list}
+										date={this._dayDate(index, todayIndex, today)}
+										focus={false}
+										id={id}
+									/>
 
-                  {dayTodos.map((todo, i) =>
-                    <SingleTodo
-                      key={i}
-                      todo={todo}
-                      complete={complete}
-                      editMode={editMode}
-                      deleteTodo={deleteTodo}
-                      textMode={textMode}
-                      textTodo={textTodo}
-                      dateTodo={dateTodo}
-                      viewAdditionalNote={this.viewAdditionalNote}
-                      showListSelector={this.showListSelector}
-                      toggleFocus={toggleFocus}
-                    />
-                  )}
-                </View>
-              : null}
-          </View>
-        )}
+									{dayTodos.map((todo, i) =>
+										<SingleTodo
+											key={i}
+											todo={todo}
+											complete={complete}
+											editMode={editMode}
+											deleteTodo={deleteTodo}
+											textMode={textMode}
+											textTodo={textTodo}
+											dateTodo={dateTodo}
+											viewAdditionalNote={this.viewAdditionalNote}
+											showListSelector={this.showListSelector}
+											toggleFocus={toggleFocus}
+										/>
+									)}
+								</View>
+							: null}
+					</View>
+				)}
 
-        <View style={{ height: 50 }} />
-      </ScrollView>
-    );
-  }
+				<View style={{ height: 50 }} />
+			</ScrollView>
+		);
+	}
 }
